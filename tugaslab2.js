41be210b1f21002ccce2c599b040e28cd5338920
hasrianaana <149997330+hasrianaana@users.noreply.github.com>
2024-06-03 10:47:08 +0800

    Add files via upload

---
+++ tugaslab2.js
@@ -0,0 +1,5 @@
+function myFunction() {
+	document.getElementById("1").innerHTML = "HASRIANA";
+	document.getElementById("2").innerHTML = "105841107623";
+	document.getElementById("3").innerHTML = "2.C IF";
+}
